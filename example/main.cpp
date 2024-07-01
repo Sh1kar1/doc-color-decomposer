848d362ef80e607bfb5f4c3a90473122870650bf
Nikita Kablov <nikita.kablov68@gmail.com>
2024-07-01 22:15:54 +0300

    build: some fixes

--- example/main.cpp
+++ example/main.cpp
@@ -1,9 +1,5 @@
 #include <doc_color_clustering/doc_color_clustering.h>
 
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-
 int main() {
   cv::Mat img = cv::imread("..\\example\\data\\doc-1.png", cv::IMREAD_COLOR);
   DocColorClustering dcc = DocColorClustering(img);
