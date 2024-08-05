6c4eddc609b646add2aa9ba148d419a9abfbb98b
Nikita Kablov <nikita.kablov68@gmail.com>
2024-08-05 04:03:52 +0300

    docs: write docstrings for public methods

--- include/doc_color_decomposer/doc_color_decomposer.h
+++ include/doc_color_decomposer/doc_color_decomposer.h
@@ -16,18 +16,67 @@
 #include <iostream>
 #include <sstream>
 
+/**
+ * @brief Interface of the [Doc Color Decomposer](https://github.com/Sh1kar1/doc-color-decomposer) library for documents decomposition by color clustering
+ */
 class [[nodiscard]] DocColorDecomposer {
 public:
+  /**
+   * @brief Constructs an empty instance
+   */
   explicit DocColorDecomposer() = default;
+
+  /**
+   * @brief Constructs an instance from the given document and pre-computes its layers
+   *
+   * @param[in] src source image of the document in the sRGB format
+   */
   explicit DocColorDecomposer(const cv::Mat& src);
 
+  /**
+   * @brief Retrieves the pre-computed layers
+   *
+   * @return list of the decomposed document layers in the sRGB format with a white background
+   */
   [[nodiscard]] std::vector<cv::Mat> GetLayers() const;
 
-  [[nodiscard]] std::string Plot3dRgb(int yaw = 115, int pitch = 15);
-  [[nodiscard]] cv::Mat Plot2dLab();
-  [[nodiscard]] std::string Plot1dPhi();
-  [[nodiscard]] std::string Plot1dClusters();
+  /**
+   * @brief Merges the pre-computed layers for testing
+   *
+   * @return image of the merged layers in the sRGB format that must be the same as the source document
+   */
   [[nodiscard]] cv::Mat MergeLayers();
+
+  /**
+   * @brief Generates a 3D scatter plot of the document colors in the linRGB space
+   *
+   * @param[in] yaw yaw-rotation angle of the view in degrees
+   * @param[in] pitch pitch-rotation angle of the view in degrees
+   *
+   * @return LaTeX code of the plot that can be saved in the .tex format and compiled
+   */
+  [[nodiscard]] std::string Plot3dRgb(int yaw = 115, int pitch = 15);
+
+  /**
+   * @brief Generates a 2D scatter plot of the document colors projections on the \f$\alpha\beta\f$ plane
+   *
+   * @return image of the plot in the sRGB format
+   */
+  [[nodiscard]] cv::Mat Plot2dLab();
+
+  /**
+   * @brief Generates a 1D histogram plot with respect to the rotation angle \f$\phi\f$
+   *
+   * @return LaTeX code of the plot that can be saved in the .tex format and compiled
+   */
+  [[nodiscard]] std::string Plot1dPhi();
+
+  /**
+   * @brief Generates a smoothed and separated by clusters 1D histogram plot with respect to the rotation angle \f$\phi\f$
+   *
+   * @return LaTeX code of the plot that can be saved in the .tex format and compiled
+   */
+  [[nodiscard]] std::string Plot1dClusters();
 
 private:
   void ComputePhiHist(int ker_size = 25);
